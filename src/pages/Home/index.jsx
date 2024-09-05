f9826ed8338b3ef9412ca69f77ef4e7372296edb
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-05 16:48:54 -0300

    arrumando home/navbar e fontes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './home.css'
+
+
+import homeImg from '../../assets/img/home/home_img.jpg'
+import logo from '../../assets/img/home/logo.svg'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className='home'>
+      <img src={homeImg} alt='' />
+      <div className='grid'>
+      
+        <nav className='navhome'>
+
+          <div className='logo'>
+            <img src={logo} alt='' />
+          </div>
+
+          <div>
+            <p>Revelamos o âmago dos negócios.</p>
+          </div>
+
+          <div>
+            <p>PT|EN</p>
+          </div>
+
+          <div>
+            <p>[Menu]</p>
+          </div>
+
+        </nav>
+
+      </div>
+
+    </section>
   )
 }
