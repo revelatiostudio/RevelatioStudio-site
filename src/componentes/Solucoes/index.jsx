933c22cdb71e7775a20c0615c5494b2bd3383156
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-06 10:28:43 -0300

    primeira parte soluçoes

---
+++ src/componentes/Solucoes/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './solucoes.css'
+
+export default function Solucoes() {
+    return (
+        <div className='grid'>
+            <div className='herofirst'>
+                <div className='left_text'>
+                    <h1>Trabalhamos</h1>
+                    <h1>com soluções de</h1>
+                </div>
+
+                <div className='right_part_solucoes'>
+
+                    <div className='time'>
+                        <p>RECIFE,BRA <a>[12:46PM]</a></p>
+                        <p>NEW YORK,EUA <a>[12:46PM]</a></p>
+                        <div className='border'></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './home.css'
 
+import Solucoes from '../../componentes/Solucoes'
+
 
 import homeImg from '../../assets/img/home/home_img.jpg'
 import logo from '../../assets/img/home/logo.svg'
@@ -8,67 +10,73 @@ import revelatio from '../../assets/img/home/name_revelatio.svg'
 
 export default function Home() {
   return (
-    <section className='home'>
-      <img src={homeImg} alt='' />
-      <div className='grid'>
+    <>
 
-        <nav className='navhome'>
+      <section className='home'>
+        <img src={homeImg} alt='' />
+        <div className='grid'>
 
-          <div className='logo'>
-            <a href='/'> <img src={logo} alt='' /></a>
-          </div>
+          <nav className='navhome'>
 
-          <div>
-            <p>Revelamos o âmago dos negócios.</p>
-          </div>
-
-          <div>
-            <p>PT|EN</p>
-          </div>
-
-          <div>
-            <p>[Menu]</p>
-          </div>
-
-        </nav>
-
-        <div className='middle'>
-          <div className='left_part'>
-
-            <div className='top_part'>
-              <p>(SOCIAL MEDIA)</p>
+            <div className='logo'>
+              <a href='/'> <img src={logo} alt='' /></a>
             </div>
 
-            <div className='bottom_part'>
-              <p>INSTAGRAM</p>
-              <p>BEHANCE</p>
+            <div>
+              <p>Revelamos o âmago dos negócios.</p>
             </div>
 
-          </div>
-
-          <div className='right_part'>
-            <div className='time_text'>
-              <p>(CURRENTLY TIME)</p>
+            <div>
+              <p>PT|EN</p>
             </div>
-            
-            <div className='time'>
-              <p>RECIFE,BRA <a>[12:46PM]</a></p>
-              <p>NEW YORK,EUA <a>[12:46PM]</a></p>
+
+            <div>
+              <p>[Menu]</p>
+            </div>
+
+          </nav>
+
+          <div className='middle'>
+            <div className='left_part'>
+
+              <div className='top_part'>
+                <p>(SOCIAL MEDIA)</p>
+              </div>
+
+              <div className='bottom_part'>
+                <p>INSTAGRAM</p>
+                <p>BEHANCE</p>
+              </div>
 
             </div>
 
+            <div className='right_part'>
+              <div className='time_text'>
+                <p>(CURRENTLY TIME)</p>
+              </div>
+
+              <div className='time'>
+                <p>RECIFE,BRA <a>[12:46PM]</a></p>
+                <p>NEW YORK,EUA <a>[12:46PM]</a></p>
+
+              </div>
+
+            </div>
+
+
           </div>
 
+          <div className='img_revelatio'>
+            <img src={revelatio} alt='' />
+
+          </div>
 
         </div>
 
-        <div className='img_revelatio'>
-          <img src={revelatio} alt='' />
+      </section>
+      <Solucoes/>
 
-        </div>
+    </>
 
-      </div>
-
-    </section>
   )
 }
