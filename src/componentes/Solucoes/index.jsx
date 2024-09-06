c167099b446334ccbce8b8feed03a2aeec91d5a4
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-06 12:09:36 -0300

    add pag soluçoes final

--- src/componentes/Solucoes/index.jsx
+++ src/componentes/Solucoes/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import prop from '../../assets/img/home/prop.png'
 import './solucoes.css'
 
 export default function Solucoes() {
@@ -19,6 +21,45 @@ export default function Solucoes() {
                     </div>
 
                 </div>
+            </div>
+            <div className='herosecond'>
+                <h1>Branding</h1>
+                <h1>& Tecnologia</h1>
+            </div>
+            <div className='herofinal'>
+
+                {/* <div className='border'></div> */}
+
+                <div className='texts_studio'>
+                    <div>
+                        <h2>[NOSSO ESTÚDIO]</h2>
+                        <p>Entregamos soluções para negócios</p>
+                        <p>que entendem que esse é o momento </p>
+                        <p>para mostrar pro mundo ao que vieram. </p>
+
+                        <div className='seecases'>
+                            <a href=''>Ver todos os cases <a>↗</a></a>
+                        </div>
+                    </div>
+
+                    <div className='studio_left'>
+                        <p className='p1'>Temos voracidade criativa e estratégica,</p>
+                        <p>concebendo cada entrega de modo único,</p>
+                        <p>entendendo a fundo as singularidades e</p>
+                        <p>necessidades, revelando e dando luz à</p>
+                        <p>propósito, sentido e resultado para as</p>
+                        <p>mais diferentes empresas e modelos de</p>
+                        <p>negócios.</p>
+                    </div>
+
+                </div>
+
+                <div className='studio_img'>
+                    <img src={prop} alt='' />
+
+                </div>
+
+
             </div>
         </div>
     )
