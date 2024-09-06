764df3baa70a2ebdbcf00afaf0dc06a2e443ea0f
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-06 12:13:08 -0300

    add lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Home from './pages/Home'
+import Lenis from '@studio-freight/lenis'
 
 function App() {
+  const lenis = new Lenis()
+
+  lenis.on('scroll', (e) => {
+    // console.log(e)
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
 
   return (
     <>
-    <Home/>
+      <Home />
     </>
   )
 }
