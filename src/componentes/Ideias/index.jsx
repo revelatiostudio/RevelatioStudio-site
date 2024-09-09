1ef90a6e11610b099b4676d84aa87cdbdf5a2ad3
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-09 14:43:14 -0300

    finalizado hover part ideias

--- src/componentes/Ideias/index.jsx
+++ src/componentes/Ideias/index.jsx
@@ -2,86 +2,86 @@ import React from 'react'
 import './ideias.css'
 
 export default function ideias() {
-  return (
-    <div className='container_ideias'>
-        <div className='grid'>
-            <div className='sections'>
-                <div className='options'>
-                    <p>01</p>
-                </div>
-                <div className='options'>
-                    <p>Ideias & Estratégias</p>
+    return (
+        <div className='container_ideias'>
+            <div className='grid'>
+                <div className='sections'>
+                    <div className='options'>
+                        <p>01</p>
+                    </div>
+                    <div className='options'>
+                        <p>Ideias & Estratégias</p>
+
+                    </div>
+
+
+                    <div className='options'>
+                        <p><span>↗</span> Posicionamento</p>
+                        <p><span>↗</span> Estratégia de Marca</p>
+                        <p><span>↗</span> Narrativa</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> Consultoria de Inovação</p>
+                        <p><span>↗</span> Tom de Voz</p>
+                        <p><span>↗</span> Identidade Verbal</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> Naming</p>
+                        <p><span>↗</span> Workshops</p>
+                        <p><span>↗</span> Mentoria</p>
+                    </div>
 
                 </div>
-                
-                
-                <div className='options'>
-                    <p><span>↗</span> Posicionamento</p>
-                    <p><span>↗</span> Estratégia de Marca</p>
-                    <p><span>↗</span> Narrativa</p>
+                <div className='sections sections_gray'>
+                    <div className='options'>
+                        <p>02</p>
+                    </div>
+                    <div className='options'>
+                        <p>Criação & Design</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> Logo System</p>
+                        <p><span>↗</span> Identidade Visual</p>
+                        <p><span>↗</span> Wireframes</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> Direção de Arte</p>
+                        <p><span>↗</span> Packaging Design</p>
+                        <p><span>↗</span> Protótipos</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> UX/UI</p>
+                        <p><span>↗</span> Website</p>
+                        <p><span>↗</span> Mockups</p>
+                    </div>
+
                 </div>
-                <div className='options'>
-                    <p><span>↗</span> Consultoria de Inovação</p>
-                    <p><span>↗</span> Tom de Voz</p>
-                    <p><span>↗</span> Identidade Verbal</p>
-                </div>
-                <div className='options'>
-                    <p><span>↗</span> Naming</p>
-                    <p><span>↗</span> Workshops</p>
-                    <p><span>↗</span> Mentoria</p>
+                <div className='sections sections_last sections_gray'>
+                    <div className='options'>
+                        <p>03</p>
+                    </div>
+                    <div className='options'>
+                        <p>Tecnologia</p>
+                    </div>
+
+                    <div className='options'>
+                        <p><span>↗</span> Front End</p>
+                        <p><span>↗</span> Back End</p>
+                        <p><span>↗</span> Automação</p>
+                    </div>
+                    <div className='options'>
+                        <p><span>↗</span> Desenvolvimento Criativo</p>
+                        <p><span>↗</span> Integração de Sistemas</p>
+                        <p><span>↗</span> Inteligência Artificial</p>
+                    </div>
+                    <div className='options'>
+                    </div>
+
                 </div>
 
             </div>
-            <div className='sections sections_gray'>
-                <div className='options'>
-                    <p>02</p>
-                </div>
-                <div className='options'>
-                    <p>Criação & Design</p>
-                </div>
-                <div className='options'>
-                    <p><span>↗</span> Logo System</p>
-                    <p><span>↗</span> Identidade Visual</p>
-                    <p><span>↗</span> Wireframes</p>
-                </div>
-                <div className='options'>
-                    <p><span>↗</span> Direção de Arte</p>
-                    <p><span>↗</span> Packaging Design</p>
-                    <p><span>↗</span> Protótipos</p>
-                </div>
-                <div className='options'>
-                    <p><span>↗</span> UX/UI</p>
-                    <p><span>↗</span> Website</p>
-                    <p><span>↗</span> Mockups</p>
-                </div>
 
-            </div>
-            <div className='sections sections_last sections_gray'>
-                <div className='options'>
-                    <p>03</p>
-                </div>
-                <div className='options'>
-                    <p>Tecnologia</p>
-                </div>
-                
-                <div className='options'>
-                    <p><span>↗</span> Front End</p>
-                    <p><span>↗</span> Back End</p>
-                    <p><span>↗</span> Automação</p>
-                </div>
-                <div className='options'>
-                    <p><span>↗</span> Desenvolvimento Criativo</p>
-                    <p><span>↗</span> Integração de Sistemas</p>
-                    <p><span>↗</span> Inteligência Artificial</p>
-                </div>
-                <div className='options'>
-                </div>
-
-            </div>
 
         </div>
-        
-      
-    </div>
-  )
+    )
 }
