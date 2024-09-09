27c93d21c7a4a7f915d980b3f52d27fde946aa82
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-09 12:01:41 -0300

    desenvolvendo parte ideias home

--- src/componentes/Ideias/index.jsx
+++ src/componentes/Ideias/index.jsx
@@ -4,7 +4,66 @@ import './ideias.css'
 export default function ideias() {
   return (
     <div className='container_ideias'>
-        Oláa
+        <div className='grid'>
+            <div className='sections'>
+                <p>01</p>
+                <p>Ideias & Estratégias</p>
+                <div className='options'>
+                    <p><span>↗</span> Posicionamento</p>
+                    <p><span>↗</span> Estratégia de Marca</p>
+                    <p><span>↗</span> Narrativa</p>
+                </div>
+                <div className='options'>
+                    <p><span>↗</span> Consultoria de Inovação</p>
+                    <p><span>↗</span> Tom de Voz</p>
+                    <p><span>↗</span> Identidade Verbal</p>
+                </div>
+                <div className='options'>
+                    <p><span>↗</span> Naming</p>
+                    <p><span>↗</span> Workshops</p>
+                    <p><span>↗</span> Mentoria</p>
+                </div>
+
+            </div>
+            <div className='sections sections_gray'>
+                <p>02</p>
+                <p>Criação & Design</p>
+                <div className='options'>
+                    <p><span>↗</span> Logo System</p>
+                    <p><span>↗</span> Identidade Visual</p>
+                    <p><span>↗</span> Wireframes</p>
+                </div>
+                <div className='options'>
+                    <p><span>↗</span> Direção de Arte</p>
+                    <p><span>↗</span> Packaging Design</p>
+                    <p><span>↗</span> Protótipos</p>
+                </div>
+                <div className='options'>
+                    <p><span>↗</span> UX/UI</p>
+                    <p><span>↗</span> Website</p>
+                    <p><span>↗</span> Mockups</p>
+                </div>
+
+            </div>
+            <div className='sections sections_last sections_gray'>
+                <p>03</p>
+                <p>Tecnologia</p>
+                <div className='options'>
+                    <p><span>↗</span> Front End</p>
+                    <p><span>↗</span> Back End</p>
+                    <p><span>↗</span> Automação</p>
+                </div>
+                <div className='options'>
+                    <p><span>↗</span> Desenvolvimento Criativo</p>
+                    <p><span>↗</span> Integração de Sistemas</p>
+                    <p><span>↗</span> Inteligência Artificial</p>
+                </div>
+                <div className='options'>
+                </div>
+
+            </div>
+
+        </div>
         
       
     </div>
