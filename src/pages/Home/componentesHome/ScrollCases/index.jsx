574edeed46dd933578b0ccd8c6884f42a8c84b8c
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-16 17:14:26 -0300

    finalizando scrollcases

--- src/pages/Home/componentesHome/ScrollCases/index.jsx
+++ src/pages/Home/componentesHome/ScrollCases/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './scrollcases.css'
 import case1 from '../../../../assets/img/home/cases/case1.png'
+import case2 from '../../../../assets/img/home/cases/case2.png'
+import case3 from '../../../../assets/img/home/cases/case3.png'
 
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -16,8 +18,8 @@ export default function ScrollCases() {
 
 
     useGSAP(() => {
-        gsap.set('.case-1',{
-            y:263
+        gsap.set('.case-1', {
+            y: 263
         })
 
 
@@ -78,11 +80,33 @@ export default function ScrollCases() {
                 <div className='overlay-blur'>
                 </div>
                 <div>
-                    
+
                 </div>
 
                 <div className='case-1'>
                     <img src={case1} alt='' />
+                    <div className='project'>
+                        <div className='project-name'>
+                            <p>Nome do projeto</p>
+                            <p>Nome do projeto</p>
+                        </div>
+                        <p>Website | Branding</p>
+
+                    </div>
+                </div>
+                <div className='case-1'>
+                    <img src={case2} alt='' />
+                    <div className='project'>
+                        <div className='project-name'>
+                            <p>Nome do projeto</p>
+                            <p>Nome do projeto</p>
+                        </div>
+                        <p>Website | Branding</p>
+
+                    </div>
+                </div>
+                <div className='case-1'>
+                    <img src={case3} alt='' />
                     <div className='project'>
                         <div className='project-name'>
                             <p>Nome do projeto</p>
@@ -104,7 +128,7 @@ export default function ScrollCases() {
                     </div>
                 </div>
                 <div className='case-1'>
-                    <img src={case1} alt='' />
+                    <img src={case2} alt='' />
                     <div className='project'>
                         <div className='project-name'>
                             <p>Nome do projeto</p>
@@ -115,29 +139,7 @@ export default function ScrollCases() {
                     </div>
                 </div>
                 <div className='case-1'>
-                    <img src={case1} alt='' />
-                    <div className='project'>
-                        <div className='project-name'>
-                            <p>Nome do projeto</p>
-                            <p>Nome do projeto</p>
-                        </div>
-                        <p>Website | Branding</p>
-
-                    </div>
-                </div>
-                <div className='case-1'>
-                    <img src={case1} alt='' />
-                    <div className='project'>
-                        <div className='project-name'>
-                            <p>Nome do projeto</p>
-                            <p>Nome do projeto</p>
-                        </div>
-                        <p>Website | Branding</p>
-
-                    </div>
-                </div>
-                <div className='case-1'>
-                    <img src={case1} alt='' />
+                    <img src={case3} alt='' />
                     <div className='project'>
                         <div className='project-name'>
                             <p>Nome do projeto</p>
@@ -149,6 +151,16 @@ export default function ScrollCases() {
                 </div>
 
                 <div className='overlay-blur bottom'>
+                </div>
+                <div className='see-cases'>
+                    <div className='all-cases'>
+                        <a href=''>Ver todos os cases <a>↗</a></a>
+                    </div>
+                    <div className='explore'>
+                        <p>Scroll to explore ↓</p>
+                        <p className='p1'>6 Cases</p>
+                    </div>
+
                 </div>
 
 
