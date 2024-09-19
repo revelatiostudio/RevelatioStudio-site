d76143311f342995c99814ffcd638918a2e5aec8
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-19 17:11:02 -0300

    scale na div

--- src/pages/About/componentesAbout/Fundadores/index.jsx
+++ src/pages/About/componentesAbout/Fundadores/index.jsx
@@ -19,9 +19,9 @@ export default function Fundadores() {
     const anotherref = useRef(null);
 
     const images = [
-        { id: 1, image: Icaro, name: "Ícaro de Souza", des1: "Co-founder", des2: "& CBO"},
-        { id: 2, image: Arthur, name: "Arthur Galvao", des1: "Co-founder", des2:"& COO" },
-        { id: 3, image: Lucas, name: "Lucas Lavor", des1: "Co-founder", des2:"& CTO"}
+        { id: 1, image: Icaro, name: "Ícaro de Souza", des1: "Co-founder", des2: "& CBO" },
+        { id: 2, image: Arthur, name: "Arthur Galvao", des1: "Co-founder", des2: "& COO" },
+        { id: 3, image: Lucas, name: "Lucas Lavor", des1: "Co-founder", des2: "& CTO" }
 
     ];
 
@@ -30,7 +30,9 @@ export default function Fundadores() {
 
         gsap.killTweensOf('.socios-img img');
         setImageHover(id)
+
         const otherImages = document.querySelectorAll(`.fund-img img:not(.image-${id})`);
+
         gsap.to(otherImages, { opacity: 0.1, filter: 'blur(5px)', duration: 0.5 });
         if (id === 1) {
             gsap.to(icaroRef.current, { opacity: 1, duration: 0.5, overwrite: true });
@@ -77,31 +79,31 @@ export default function Fundadores() {
 
                     <div className='fund-name icaro' ref={icaroRef} style={{ opacity: 0 }}>
                         <h2>Ícaro de Souza</h2>
-                        <p>Chief Operations Officer no Revelatio <br/> 
-                           Studio®, focado em toda estratégia, <br/>
-                           <br/>
-                           gestão e criação visual dos projetos,<br/> 
-                           especialista em vendas e comunicação.</p>
+                        <p>Chief Operations Officer no Revelatio <br />
+                            Studio®, focado em toda estratégia, <br />
+                            <br />
+                            gestão e criação visual dos projetos,<br />
+                            especialista em vendas e comunicação.</p>
                     </div>
 
                     <div className='fund-name arthur' ref={arthurRef} style={{ opacity: 0 }}>
                         <h2>Arthur Galvão</h2>
-                        <p>Chief Brand Officer no Revelatio Studio®. <br/>
-                           Lidera os projetos de marca do estúdio. <br/>
-                           <br/>
-                           Designer especialista em Branding, <br/>
-                           focado nas áreas de naming, estratégia <br/>
-                           de marca e identidade visual.</p>
+                        <p>Chief Brand Officer no Revelatio Studio®. <br />
+                            Lidera os projetos de marca do estúdio. <br />
+                            <br />
+                            Designer especialista em Branding, <br />
+                            focado nas áreas de naming, estratégia <br />
+                            de marca e identidade visual.</p>
                     </div>
 
                     <div className='fund-name lucas' ref={lucasRef} style={{ opacity: 0 }}>
                         <h2>Lucas Lavor</h2>
-                        <p>Chief Technology Officer no Revelatio <br/> 
-                           Studio®. Desenvolvedor fullstack com <br/> 
-                           <br/>
-                           ênfase em webdesign e apps. Lidera <br/>
-                           iniciativas tecnológicas e assegura a <br/>
-                           excelência em todos os projetos.
+                        <p>Chief Technology Officer no Revelatio <br />
+                            Studio®. Desenvolvedor fullstack com <br />
+                            <br />
+                            ênfase em webdesign e apps. Lidera <br />
+                            iniciativas tecnológicas e assegura a <br />
+                            excelência em todos os projetos.
                         </p>
                     </div>
 
@@ -110,45 +112,29 @@ export default function Fundadores() {
 
                 </div>
                 <div className='fund-img'>
-                    {
-                        images.map((image) => (
-                            <div>
-                                <motion.img
-                                    key={image.id}
-                                    src={image.image} alt=''
-                                    className={`image-${image.id}`}
-                                    onMouseEnter={() => handleMouseEnter(image.id)}
-                                    onMouseLeave={handleMouseLeave}
-                                    initial={{ opacity: 1, filter: 'blur(0px)' }}
-                                    whileHover={{ scale: 1.1, opacity: 1, filter: 'blur(0px)' }}
-                                    transition={{ duration: 0.5 }}
-
-                                />
-                                <div className='founder'>
-                                    <p>{image.name}</p>
-                                    <div className='description'>
-                                        <p><span>{image.des1}</span></p>
-                                        <p><span>{image.des2}</span></p>
-                                    </div>
-
-                                    
-
+                    {images.map((image) => (
+                        <motion.div
+                            key={image.id}
+                            className='one-founder'
+                            initial={{ opacity: 1, filter: 'blur(0px)' }}
+                            whileHover={{ scale: 1.1, opacity: 1, filter: 'blur(0px)' }}
+                            transition={{ duration: 0.5 }}>
+                                
+                            <motion.img
+                                src={image.image} alt=''
+                                className={`image-${image.id}`}
+                                onMouseEnter={() => handleMouseEnter(image.id)}
+                                onMouseLeave={handleMouseLeave}
+                            />
+                            <div className='founder'>
+                                <p>{image.name}</p>
+                                <div className='description'>
+                                    <p><span>{image.des1}</span></p>
+                                    <p><span>{image.des2}</span></p>
                                 </div>
-
-
-
                             </div>
-
-
-
-
-                        ))
-
-
-                    }
-
-
-
+                        </motion.div>
+                    ))}
                 </div>
 
             </div>
