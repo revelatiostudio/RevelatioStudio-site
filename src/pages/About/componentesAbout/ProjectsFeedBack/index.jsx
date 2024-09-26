51b3e3c316c95b64e80011343f3b67ff65ba4705
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-26 18:30:12 -0300

    colocando componente dentro da grid

--- src/pages/About/componentesAbout/ProjectsFeedBack/index.jsx
+++ src/pages/About/componentesAbout/ProjectsFeedBack/index.jsx
@@ -2,42 +2,69 @@ import React from 'react'
 import './projectsfeedback.css'
 import feedback1 from '../../../../assets/img/about/feedback1.png'
 import arthurMedeiros from '../../../../assets/img/about/arthur-medeiros.png'
+import setaEsquerda from '../../../../assets/img/about/setaEsc.svg'
+import setaDireita from '../../../../assets/img/about/setaDir.svg'
+import { Progress } from "@nextui-org/react";
 
 export default function ProjectsFeedBack() {
   return (
-    <section className='project-feedback'>
-      <div className='img-project'>
-        <img src={feedback1} alt='projeto1' />
+    <div className='grid-global'>
+      <section className='project-feedback'>
 
-      </div>
 
-      <div className='feedback'>
-        <div className='people-description'>
-          <div className='people-img'>
-            <img src={arthurMedeiros} alt='Arthur Medeiros imagem' />
+
+        <div className='img-project'>
+          <img src={feedback1} alt='projeto1' />
+
+        </div>
+
+        <div className='feedback'>
+          <div className='people-description'>
+            <div className='people-img'>
+              <img src={arthurMedeiros} alt='Arthur Medeiros imagem' />
+
+            </div>
+            <div className='people-name'>
+              <p>Arthur Medeiros</p>
+              <p><span>Brand director - Atho Studio</span></p>
+
+            </div>
 
           </div>
-          <div className='people-name'>
-            <p>Arthur Medeiros</p>
-            <p><span>Brand director - Atho Studio</span></p>
+
+          <div className='descri'>
+            <p>Go further into Planet Earth’s most out-there <br />
+              playgrounds with technical gear designed for  <br />
+              mixed terrains and trails. Engineered with highly  <br />
+              durable yet comfortable fabrics that combine the  <br />
+              incredible properties of soft touch, sweat-wicking <br />
+              and breathable materials with the toughness to  <br />
+              keep going as long as you can. Wild times await.</p>
+
+          </div>
+
+          <div className='change-slide'>
+            <div className='bar'>
+              <Progress classNames={{
+                base: 'h-[0.1rem]'
+
+              }}
+                size="sm" aria-label="Loading..."
+                color="default" value={10} radius='none' minValue={0} maxValue={100} valueLabel={10}
+              />
+
+            </div>
+            <div className='setas'>
+              <img src={setaEsquerda} alt='' />
+              <img src={setaDireita} alt='' />
+
+            </div>
 
           </div>
 
         </div>
 
-        <div className='descri'>
-          <p>Go further into Planet Earth’s most out-there <br />
-            playgrounds with technical gear designed for  <br />
-            mixed terrains and trails. Engineered with highly  <br />
-            durable yet comfortable fabrics that combine the  <br />
-            incredible properties of soft touch, sweat-wicking <br />
-            and breathable materials with the toughness to  <br />
-            keep going as long as you can. Wild times await.</p>
-
-        </div>
-
-      </div>
-
-    </section>
+      </section>
+    </div>
   )
 }
