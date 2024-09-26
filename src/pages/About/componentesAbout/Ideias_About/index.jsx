3a9a1dcff6155a12e8d2a752262f70c1c1fd09eb
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-26 14:36:54 -0300

    organizando css

--- src/pages/About/componentesAbout/Ideias_About/index.jsx
+++ src/pages/About/componentesAbout/Ideias_About/index.jsx
@@ -6,20 +6,20 @@ import Ideias from '../../../../componentes/Ideias'
 export default function Ideias_About() {
   return (
     <section>
-        <div className='grid-global'>
-            <div className='only'>
-                <h1> O que nos <br/>faz únicos?</h1>
-                <div>
-                    <p>Nossa constante expansão de referências é um reflexo <br/> 
-                    da nossa <span>imersão contínua</span> no universo em que atuamos. <br/> 
-                    <span>Estamos sempre inquietos</span>, buscando as melhores soluções <br/>
-                    para pessoas, marcas e negócios. A <span>idoneidade é o alicerce</span> que <br/>
-                    sustenta todos os nossos processos, parceiros e colaboradores.</p>
-                </div>
-            </div>
+      <div className='grid-global'>
+        <div className='only'>
+          <h1> O que nos <br />faz únicos?</h1>
+          <div>
+            <p>Nossa constante expansão de referências é um reflexo <br />
+              da nossa <span>imersão contínua</span> no universo em que atuamos. <br />
+              <span>Estamos sempre inquietos</span>, buscando as melhores soluções <br />
+              para pessoas, marcas e negócios. A <span>idoneidade é o alicerce</span> que <br />
+              sustenta todos os nossos processos, parceiros e colaboradores.</p>
+          </div>
         </div>
-        <Ideias color='white-ideias'/>
+      </div>
+      <Ideias color='white-ideias' />
     </section>
-  
+
   )
 }
