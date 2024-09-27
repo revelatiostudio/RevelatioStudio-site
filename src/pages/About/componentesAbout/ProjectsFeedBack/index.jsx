da70f766eb72dca1f902d3f059cfc78020e9f1f2
gabriel <gabrieldecarvalhu@gmail.com>
2024-09-27 15:57:44 -0300

    desenvolvendo animaçao na troca de img

--- src/pages/About/componentesAbout/ProjectsFeedBack/index.jsx
+++ src/pages/About/componentesAbout/ProjectsFeedBack/index.jsx
@@ -1,20 +1,86 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './projectsfeedback.css'
 import feedback1 from '../../../../assets/img/about/feedback1.png'
+import lucas from '../../../../assets/img/about/Lucas.png'
+import icaro from '../../../../assets/img/about/Icaro.png'
+
 import arthurMedeiros from '../../../../assets/img/about/arthur-medeiros.png'
 import setaEsquerda from '../../../../assets/img/about/setaEsc.svg'
 import setaDireita from '../../../../assets/img/about/setaDir.svg'
-import { Progress } from "@nextui-org/react";
+import { image, Progress } from "@nextui-org/react";
+
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react'
 
 export default function ProjectsFeedBack() {
+
+  const image = [
+    { id: 1, img: feedback1 },
+    { id: 2, img: lucas },
+    { id: 3, img: icaro }
+  ]
+
+  let currentImg = 0;
+  const totalSlides = 2;
+
+
+
+  const updateImage = (direction) => { 
+    const images = [...document.querySelectorAll('.img-project img')];
+
+    if(direction === "left" & currentImg <= (totalSlides - 1)){
+      console.log('left',currentImg)
+      gsap.fromTo(
+        images[currentImg],
+        {
+          clipPath:
+            "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)" // para esquerda
+        },
+        {
+          clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+          duration: 1.5,
+          ease: "hop",
+        }
+      );
+      currentImg++;
+
+    }
+
+    if(direction === "rigth" & currentImg >= (totalSlides -1)){
+      console.log('rigth',currentImg)
+      
+      gsap.to(images[currentImg - 1],{
+        clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+        duration: 1.5,
+        ease: "hop",
+      },
+      
+    )
+    currentImg--;
+    
+    }
+
+ 
+  }
+
+
+
+
+
+
   return (
     <div className='grid-global'>
       <section className='project-feedback'>
 
-
-
         <div className='img-project'>
-          <img src={feedback1} alt='projeto1' />
+
+          {
+
+            image.map((imagem, index) => (
+              <img src={imagem.img} alt='' key={index} />
+
+            ))
+          }
 
         </div>
 
@@ -54,9 +120,9 @@ export default function ProjectsFeedBack() {
               />
 
             </div>
-            <div className='setas'>
-              <img src={setaEsquerda} alt='' />
-              <img src={setaDireita} alt='' />
+            <div className='setas' >
+              <img src={setaEsquerda} alt='' onClick={() => updateImage('left')} />
+              <img src={setaDireita} alt='' onClick={() => updateImage('rigth')} />
 
             </div>
 
