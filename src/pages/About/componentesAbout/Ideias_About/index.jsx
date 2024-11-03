75e9e41943d83139fd52ddedf13052369dacc815
gabriel barbosa <gabrieldecarvalhu@gmail.com>
2024-11-03 15:42:58 -0300

    ideais - 800px

--- src/pages/About/componentesAbout/Ideias_About/index.jsx
+++ src/pages/About/componentesAbout/Ideias_About/index.jsx
@@ -10,11 +10,11 @@ export default function Ideias_About() {
       <div className='grid-global'>
         <div className='only'>
           <h1> O que nos <br />faz únicos?</h1>
-          <div>
-            <p>Nossa constante expansão de referências é um reflexo <br />
-              da nossa <span>imersão contínua</span> no universo em que atuamos. <br />
-              <span>Estamos sempre inquietos</span>, buscando as melhores soluções <br />
-              para pessoas, marcas e negócios. A <span>idoneidade é o alicerce</span> que <br />
+          <div className='ideia-text'>
+            <p>Nossa constante expansão de referências é um reflexo <span className='hide-on-mobile'><br /></span>
+              da nossa <span>imersão contínua</span> no universo em que atuamos. <span className='hide-on-mobile'><br /></span>
+              <span>Estamos sempre inquietos</span>, buscando as melhores soluções <span className='hide-on-mobile'><br /></span>
+              para pessoas, marcas e negócios. A <span>idoneidade é o alicerce</span> que <span className='hide-on-mobile'><br /></span>
               sustenta todos os nossos processos, parceiros e colaboradores.</p>
           </div>
         </div>
