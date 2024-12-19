489e24205753b4462ce49904b45e83b581fc71b3
Gabriel <gabrieldecarvalhu@gmail.com>
2024-12-19 12:47:29 -0300

    add hora/local outras páginas

--- src/componentes/Solucoes/index.jsx
+++ src/componentes/Solucoes/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import img_solucoes from '../../assets/img/home/img_solucoes.png'
 import blour from '../../assets/img/home/blour.png'
@@ -13,6 +13,10 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Solucoes() {
 
+    const [horaRecife, setHoraRecife] = useState('');
+    const [horaLocal, setHoraLocal] = useState('');
+    const [idioma, setIdioma] = useState('')
+
     gsap.registerPlugin(useGSAP, ScrollTrigger);
 
     function tiraBlour() {
@@ -20,7 +24,7 @@ export default function Solucoes() {
             scrollTrigger: {
                 trigger: '.container-solucoes',
                 start: 'top top',
-                end:'+=800',
+                end: '+=800',
                 scrub: 1,
             }
         });
@@ -37,6 +41,70 @@ export default function Solucoes() {
     })
 
 
+
+
+
+
+
+    const obterHoraRecife = () => {
+        const horaRecife = new Date().toLocaleString("pt-BR", {
+            timeZone: "America/Recife",
+            timeZoneName: "short",
+            hour12: false
+        });
+
+        var hora = horaRecife.split(' ')
+        hora = hora[1].split(':')
+        var horaCerta = hora[0] + ":" + hora[1]
+        const comparaHora = hora[0]
+
+
+        if (Number(comparaHora) <= 11) {
+            setHoraRecife(horaCerta + " " + "AM")
+        } else {
+            setHoraRecife(horaCerta + " " + "PM")
+        }
+
+    };
+
+    const obterHoraLocal = () => {
+        const localTime = new Date().toLocaleString("pt-BR", {
+            timeZoneName: "short",
+            hour12: false
+        });
+
+        var hora = localTime.split(' ')
+        hora = hora[1].split(':')
+        var horaCerta = hora[0] + ":" + hora[1]
+        const comparaHora = hora[0]
+
+
+        if (Number(comparaHora) <= 11) {
+            setHoraLocal(horaCerta + " " + "AM")
+        } else {
+            setHoraLocal(horaCerta + " " + "PM")
+        }
+        let idioma = navigator.language || navigator.userLanguage;
+        const pcIdioma = idioma.split('-')[1]
+        setIdioma(pcIdioma)
+
+    };
+
+
+
+    useEffect(() => {
+        obterHoraRecife()
+        obterHoraLocal()
+
+        const intervalo = setInterval(() => {
+            obterHoraRecife();
+        }, 30000);
+
+        return () => clearInterval(intervalo);
+
+    }, [horaRecife])
+
+
     return (
         <section className='container-solucoes'>
             <div className='grid-global'>
@@ -49,8 +117,8 @@ export default function Solucoes() {
                     <div className='right_part_solucoes'>
 
                         <div className='time'>
-                            <p>RECIFE,BRA <a>[12:46PM]</a></p>
-                            <p>NEW YORK,EUA <a>[12:46PM]</a></p>
+                            <p>RECIFE,BR <a>[{horaRecife}]</a></p>
+                            <p>LOCAL,{idioma} <a>[{horaLocal}]</a></p>
                             <div className='border-time'></div>
                         </div>
 
@@ -71,16 +139,16 @@ export default function Solucoes() {
                             <p>que entendem que esse é o momento </p>
                             <p>para mostrar pro mundo ao que vieram. </p>
 
-                            
-                        <div className='studio_left studio-res'>
-                            <p className='p1'>Temos voracidade criativa e estratégica,</p>
-                            <p>concebendo cada entrega de modo único,</p>
-                            <p>entendendo a fundo as singularidades e</p>
-                            <p>necessidades, revelando e dando luz à</p>
-                            <p>propósito, sentido e resultado para as</p>
-                            <p>mais diferentes empresas e modelos de</p>
-                            <p>negócios.</p>
-                        </div>
+
+                            <div className='studio_left studio-res'>
+                                <p className='p1'>Temos voracidade criativa e estratégica,</p>
+                                <p>concebendo cada entrega de modo único,</p>
+                                <p>entendendo a fundo as singularidades e</p>
+                                <p>necessidades, revelando e dando luz à</p>
+                                <p>propósito, sentido e resultado para as</p>
+                                <p>mais diferentes empresas e modelos de</p>
+                                <p>negócios.</p>
+                            </div>
 
                             <div className='seecases'>
                                 <a href=''>Ver todos os cases</a>
