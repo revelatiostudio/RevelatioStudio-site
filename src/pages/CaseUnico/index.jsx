6a88d8bd2b5e9d85002cd018a0ece5434f53e73b
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-01-29 16:00:02 -0300

    tratando caso de campos vazios no case

--- src/pages/CaseUnico/index.jsx
+++ src/pages/CaseUnico/index.jsx
@@ -30,40 +30,35 @@ const CaseUnico = () => {
 
     const data = document?.find((_, index) => index === parseInt(id)) &&
     {
-        title: document[id]?.data.title[0].text,
-        description: document[id]?.data.description[0].text,
-        hero_image: document[id]?.data.hero_image.url,
-        background: document[id]?.data.background[0].text,
+        title: document[id]?.data.title[0]?.text || "",
+        description: document[id]?.data.description[0]?.text || "",
+        hero_image: document[id]?.data.hero_image?.url || "",
+        background: document[id]?.data.background[0]?.text || "",
         group_images_one: {
-            g1_image_1: document[id]?.data.group_images_one[0].g1_image_1.url,
-            g1_image_2: document[id]?.data.group_images_one[0].g1_image_2.url,
-            g1_image_3: document[id]?.data.group_images_one[0].g1_image_3.url,
+            g1_image_1: document[id]?.data.group_images_one[0].g1_image_1?.url || "",
+            g1_image_2: document[id]?.data.group_images_one[0].g1_image_2?.url || "",
+            g1_image_3: document[id]?.data.group_images_one[0].g1_image_3?.url || "",
         },
         branding_group: {
-            brand_naming: document[id]?.data.branding_group[0].brand_naming[0].text,
-            brand_positioning: document[id]?.data.branding_group[0].brand_positioning[0].text
+            brand_naming: document[id]?.data.branding_group[0].brand_naming[0]?.text || "",
+            brand_positioning: document[id]?.data.branding_group[0].brand_positioning[0]?.text || ""
         },
         group_images_two: {
-            g2_image_1: document[id]?.data.group_images_two[0].g2_image_1.url,
-            g2_image_2: document[id]?.data.group_images_two[0].g2_image_2.url,
-            g2_image_3: document[id]?.data.group_images_two[0].g2_image_3.url,
+            g2_image_1: document[id]?.data.group_images_two[0]?.g2_image_1.url || "",
+            g2_image_2: document[id]?.data.group_images_two[0]?.g2_image_2.url || "",
+            g2_image_3: document[id]?.data.group_images_two[0]?.g2_image_3.url || "",
 
 
         },
-        visual_expression: document[id]?.data.visual_expression[0].text,
+        visual_expression: document[id]?.data.visual_expression[0]?.text || "",
         group_images_three: {
-            g3_image_1: document[id]?.data.group_images_three[0].g3_image_1.url,
-            g3_image_2: document[id]?.data.group_images_three[0].g3_image_2.url,
-            g3_image_3: document[id]?.data.group_images_three[0].g3_image_3.url,
+            g3_image_1: document[id]?.data.group_images_three[0]?.g3_image_1.url || "",
+            g3_image_2: document[id]?.data.group_images_three[0]?.g3_image_2.url || "",
+            g3_image_3: document[id]?.data.group_images_three[0]?.g3_image_3.url || "",
 
         },
-        categories: document[id]?.data.categories,
-        // categories:{
-        //     item1: document[id]?.data.categories[0].text,
-        //     item2: document[id]?.data.categories[1].text,
-        //     item3: document[id]?.data.categories[2].text
-        // },
-        about_the_project: document[id]?.data.about_the_project[0].text
+        categories: document[id]?.data?.categories || "",
+        about_the_project: document[id]?.data.about_the_project[0]?.text || ""
 
 
 
