d5b9f671907cca13e756a23815914a68db012945
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-02-01 19:14:32 -0300

    criando animaçao nova dos services e ajustando

--- src/componentes/Services/index.jsx
+++ src/componentes/Services/index.jsx
@@ -1,19 +1,41 @@
 import './services.css'
-export default function Services () {
-  return(
+import { motion } from "framer-motion"
+
+export default function Services() {
+  return (
     <section className="container-services">
       <div className='grid-global'>
         <div className='container-services'>
-          <div className='service'>
-            <div className='bullets'>
-              <p>Jornada</p>
-            </div>
-            <div className='points'>
-              <p>Anos de Mercado</p>
-              <h1>04</h1>
-            </div>
-          </div>
-          <div className='service'>
+
+
+         
+      
+       
+            <motion.div 
+            className='service'
+            layout
+            initial={{ height: "70px" }}
+            animate={{ height: "70px" }}
+            whileHover={{ height: "200px" }}
+            transition={{ type: "spring", stiffness: 200 }}
+            >
+              <div className='bullets'>
+                <p>Jornada</p>
+              </div>
+              <div className='points'>
+                <p>Anos de Mercado</p>
+                <h1>04</h1>
+              </div>
+            </motion.div>
+          
+          <motion.div  
+          className='service'
+          layout
+          initial={{ height: "70px" }}
+          animate={{ height: "70px" }}
+          whileHover={{ height: "200px" }}
+          transition={{ type: "spring", stiffness: 200 }}
+          >
             <div className='bullets'>
               <p>Impacto</p>
             </div>
@@ -21,7 +43,43 @@ export default function Services () {
               <p>Atuação Global</p>
               <h1>06</h1>
             </div>
-          </div>
+          </motion.div >
+
+          <motion.div  
+          className='service'
+          layout
+          initial={{ height: "70px" }}
+          animate={{ height: "70px" }}
+          whileHover={{ height: "200px" }}
+          transition={{ type: "spring", stiffness: 200 }}
+          >
+            <div className='bullets'>
+              <p>Experiência</p>
+            </div>
+            <div className='points'>
+              <p>Projetos Realizados</p>
+              <h1>+300</h1>
+            </div>
+          </motion.div>
+
+          <motion.div  
+          className='service'
+          layout
+          initial={{ height: "70px" }}
+          animate={{ height: "70px" }}
+          whileHover={{ height: "200px" }}
+          transition={{ type: "spring", stiffness: 200 }}
+          >
+            <div className='bullets'>
+              <p>Chancela</p>
+            </div>
+            <div className='points'>
+              <p>Reconhecimentos Internacionais</p>
+              <h1>33</h1>
+            </div>
+          </motion.div >
+
+
         </div>
 
       </div>
