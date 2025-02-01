9acfcc6a9024dbfd385417102a88541394f2a125
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-02-01 18:26:47 -0300

    base estatica da nova parte de services

--- src/componentes/Services/index.jsx
+++ src/componentes/Services/index.jsx
@@ -1,106 +1,30 @@
-import React from 'react'
 import './services.css'
-
-import prop from '../../assets/img/home/prop.png'
-
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-export default function Services() {
-  gsap.registerPlugin(useGSAP, ScrollTrigger);
-
-  function scrollhover() {
-    const texts = [...document.querySelectorAll('.lista_images')];
-    texts.forEach((text) => {
-      ScrollTrigger.create({
-        trigger: text,
-        start: "top center",
-        end: "bottom center",
-        toggleClass: { targets: text, className: "active" },
-        onEnter: () => text.classList.add("active"),
-        onLeave: () => text.classList.remove("active"),
-        onEnterBack: () => text.classList.add("active"),
-        onLeaveBack: () => text.classList.remove("active"),
-      });
-    });
-
-
-  }
-
-  useGSAP(() => {
-    scrollhover();
-
-  })
-
-  return (
-    <div className='grid-global'>
-
-      <div className='container'>
-
-        <div className='images_services'>
-
-          <div className='lista_images'>
-            <div className='text'>
-              <h1>+04</h1>
+export default function Services () {
+  return(
+    <section className="container-services">
+      <div className='grid-global'>
+        <div className='container-services'>
+          <div className='service'>
+            <div className='bullets'>
+              <p>Jornada</p>
             </div>
-
-            <div className='images'>
-              <img src={prop} alt='' />
-            </div>
-            <div className='text_solutions'>
-              <p>Anos gerando soluções</p>
-              <p>criativas para o mercado</p>
+            <div className='points'>
+              <p>Anos de Mercado</p>
+              <h1>04</h1>
             </div>
           </div>
-
-          <div className='lista_images'>
-            <div className='text'>
-              <h1>+05</h1>
-
+          <div className='service'>
+            <div className='bullets'>
+              <p>Impacto</p>
             </div>
-            <div className='images'>
-              <img src={prop} alt='' />
-            </div>
-            <div className='text_solutions'>
-              <p>Países em que nosso</p>
-              <p>trabalho está presente</p>
+            <div className='points'>
+              <p>Atuação Global</p>
+              <h1>06</h1>
             </div>
           </div>
-          <div className='lista_images'>
-            <div className='text'>
-              <h1>+27</h1>
-
-            </div>
-            <div className='images'>
-              <img src={prop} alt='' />
-            </div>
-            <div className='text_solutions'>
-              <p>Projetos com destaque e </p>
-              <p>reconhecimento internacional</p>
-            </div>
-          </div>
-          <div className='lista_images'>
-            <div className='text'>
-              <h1>+200</h1>
-
-            </div>
-            <div className='images'>
-              <img src={prop} alt='' />
-            </div>
-            <div className='text_solutions'>
-              <p>Projetos realizados ao </p>
-              <p>redor do mundo</p>
-            </div>
-          </div>
-
         </div>
 
-
-
       </div>
-
-    </div>
+    </section>
   )
 }
