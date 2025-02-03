08a99d3210555471fd5aa97cc33516e52ebd362f
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-02-03 16:11:36 -0300

    atualizando nova aboutHome

--- src/pages/Home/componentesHome/AboutHome/index.jsx
+++ src/pages/Home/componentesHome/AboutHome/index.jsx
@@ -4,44 +4,46 @@ import './abouthome.css'
 export default function AboutHome() {
     return (
 
-        <section className='container_about'>
+        <section className='container-about'>
             <div className='grid-global'>
-                <div className='first_text'>
-                    <h1>Alimentando o <span> caos </span> <span>que existe</span> <span>em<br></br>toda ordem.</span>
-                        <span>Coordenando</span> <span>a</span> ordem <br></br>que existe em todo caos.</h1>
-                </div>
-
-                <div className='first_text-mobile'>
-                    <h1>Alimentando o <span> caos </span> <span>que existe</span> <span>em <span>toda</span> ordem.</span>
-                        <span>Coordenando</span> <span>a</span> ordem que existe em todo caos.</h1>
-                </div>
-
-                <div className='about_home'>
-                    <div className='about_right'>
-
+                <div className='container-manifesto'>
+                    <div className='manifesto'>
+                        <h2>[ NOSSO MANIFESTO ]</h2>
                     </div>
+                    <div className='manifesto-text'>
+                        <h1>
+                            Alimentando o caos que <br /> existe em toda ordem. <br /> Coordenando a ordem <br /> que existe em todo caos.
+                        </h1>
 
-                    <div className='about_text'>
-                        <p className='spacing'>Não apenas criamos.</p>
-                        <p>Nós recriamos, reviramos a casa, vasculhamos gavetas,</p>
-                        <div className='text_ladder'>
-                            <p className='p1'>colocamos</p>
-                            <p className='p2'>tudo</p>
-                            <p className='p3'>abaixo.</p>
+                        <div className='squares'>
+                            <div className='square-one'>
+                                <p>
+                                    Não apenas criamos. Nós recriamos, reviramos <br />
+                                    a casa, vasculhamos gavetas, colocamos tudo <br />
+                                    abaixo. Causamos incômodo, instauramos o <br />
+                                    caos, mas tudo isso de forma sistemática para <br />
+                                    revelar o verdadeiro sentido das coisas.
+                                </p>
+                            </div>
+
+                            <div className='square-two'>
+                            <p>
+                                Não fazemos algo apenas por vaidade ou <br />
+                                capricho, temos uma nata "imparabilidade" <br />
+                                até encontrar função, beleza e autenticidade <br />
+                                em nossas criações, que transcendem até <br />
+                                mesmo nossa própria existência.
+                            </p>
                         </div>
-                        <p>Causamos incômodo, instauramos o caos, mas tudo isso de forma</p>
-                        <p className='spacing'>sistemática para revelar o <span className='text_white'> verdadeiro sentido das coisas.</span></p>
+                        </div>
 
-                        <p>Não fazemos algo apenas por vaidade ou capricho, temos uma</p>
-                        <p>nata "imparabilidade" até encontrar <span className='text_white'>função, beleza e autenticidade</span></p>
-                        <p className='spacing'>em nossas criações, que transcendem até mesmo</p>
-                        <p>nossa própria existência.</p>
+
+
                     </div>
 
                 </div>
+
             </div>
-
-
         </section>
 
 
