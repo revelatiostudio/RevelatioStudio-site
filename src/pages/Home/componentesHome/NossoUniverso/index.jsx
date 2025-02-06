d366de386b97ff919f913dde1d8b73ead4fd8647
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-02-06 12:04:17 -0300

    arrumando scroll

--- src/pages/Home/componentesHome/NossoUniverso/index.jsx
+++ src/pages/Home/componentesHome/NossoUniverso/index.jsx
@@ -5,37 +5,49 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import SplitType from 'split-type'
+import { useRef, useState } from 'react';
 
 export default function NossoUniverso() {
     gsap.registerPlugin(useGSAP, ScrollTrigger);
+    const nossouniversoRef = useRef(null)
+    const [escala, setEscala] = useState(1)
 
 
     function separaLetras() {
+        const container = nossouniversoRef.current
         const title = new SplitType(".container-nosso-universo h1", { types: 'words, chars' })
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: ".container-nosso-universo",
-                markers: false,
+                end: () => "+=" + container.offsetHeight * 2.5,
+                markers: true,
                 scrub: 1,
                 pin: true,
-                ease: "none"
-                
+                ease: "none",
+
             }
         })
 
         gsap.set(title.chars, {
-            letterSpacing: "160px"
+            letterSpacing: "160px",
         })
 
         tl.to(title.chars, {
-            letterSpacing: "5px"
-        }).to(".container-nosso-universo h1",{
-            transformOrigin:"48% 50%",
+            letterSpacing: "5px",
+            duration: 3,
+            ease: "none",
+        }).to(".container-nosso-universo h1", {
+            transformOrigin: "48% 50%",
             scale: 500,
             stagger: 0.25,
             duration: 3,
             ease: "none",
+
+
         })
+
+
+
 
 
 
@@ -48,7 +60,7 @@ export default function NossoUniverso() {
 
 
     return (
-        <section className='container-nosso-universo'>
+        <section className='container-nosso-universo' ref={nossouniversoRef}>
             <h1>NOSSO  UNIVERSO</h1>
 
         </section>
