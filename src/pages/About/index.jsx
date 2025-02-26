4c23a0308d695daf7362399316c73c852af54443
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-02-26 15:19:03 -0300

    mudando header de about até 1150px

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -5,7 +5,7 @@ import Services from '../../componentes/Services'
 import './about.css'
 
 
-import about_img from '../../assets/img/home/home_img.jpg'
+import about_img from '../../assets/img/about/about-img.jpg'
 import img_blour from '../../assets/img/about/imgBlour.png'
 import Fundadores from './componentesAbout/Fundadores'
 import RoletaScroll from './componentesAbout/RoletaScroll'
@@ -25,15 +25,17 @@ const About = () => {
     <>
       <header>
         <div className='grid-global'>
-          {/* 
-          <NavBar color='black' /> */}
-          <div className='about-us'>
-            <h1>Sobre nós</h1>
-            <div className='about-right'>
-              <h2>Somos uma boutique de design focada <br />
-                em soluções de <span>branding & tecnologia</span> <br /> para empresas em crescimento.</h2>
-            </div>
+          <div className='aa'>
+            <div className='about-us'>
+              <div className='about-text-page'>
+                <h1>Sobre nós</h1>
+              </div>
+              <div className='about-right'>
+                <h2>Somos uma boutique de design focada <br />
+                  em soluções de <span>branding & tecnologia</span> <br /> para empresas em crescimento.</h2>
+              </div>
 
+            </div>
           </div>
         </div>
         <div className='img-about'>
@@ -42,12 +44,8 @@ const About = () => {
         </div>
       </header>
 
-
-      <Solucoes />
-      <Services />
       <Fundadores />
       <RoletaScroll />
-      <Ideias_About />
       <ProjectsFeedBack />
       <InfinitTimeLine />
       <Ambiente color='white' />
