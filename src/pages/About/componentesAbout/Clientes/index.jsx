6051759caff2a339abb12f9e802f279ee35de697
GabrielBarbosa2003 <gabrieldecarvalhu@gmail.com>
2025-03-17 10:43:03 -0300

    mudando novas imagens de clientes

--- src/pages/About/componentesAbout/Clientes/index.jsx
+++ src/pages/About/componentesAbout/Clientes/index.jsx
@@ -1,8 +1,32 @@
 import React, { useRef } from 'react'
 import './clientes.css'
-import ibm from '../../../../assets/img/about/clientes/ibm-logo.svg'
-import navigator from '../../../../assets/img/about/clientes/navigator-logo.svg'
-import unileven from '../../../../assets/img/about/clientes/unileven-logo.svg'
+
+import acaso from '../../../../assets/img/about/clientes/acaso.svg'
+import ani from '../../../../assets/img/about/clientes/ani.svg'
+import carvalheira from '../../../../assets/img/about/clientes/carvalheira.png'
+import coc from '../../../../assets/img/about/clientes/coc.png'
+import hsm from '../../../../assets/img/about/clientes/hsm.png'
+import loc from '../../../../assets/img/about/clientes/loc.png'
+import loomi from '../../../../assets/img/about/clientes/loomi.svg'
+import PeG from '../../../../assets/img/about/clientes/p&g.png'
+import sas from '../../../../assets/img/about/clientes/sas.svg'
+import seek from '../../../../assets/img/about/clientes/seek.svg'
+import serious from '../../../../assets/img/about/clientes/serious.svg'
+import serur from '../../../../assets/img/about/clientes/serur.svg'
+import singu from '../../../../assets/img/about/clientes/singu.svg'
+import trois from '../../../../assets/img/about/clientes/trois.svg'
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -32,49 +56,49 @@ export default function Clientes() {
 
                 <div className='grid-clients'>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={acaso} alt='acaso-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={ani} alt='ani-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={carvalheira} alt='carvalheira-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={coc} alt='coc-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={hsm} alt='hsm-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={loc} alt='loc-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={loomi} alt='loomi-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={PeG} alt='PeG-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={sas} alt='sas-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={seek} alt='seek-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={serious} alt='serious-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={serur} alt='serur-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={singu} alt='singu-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={navigator} alt='navigator-logo' />
+                        <img src={trois} alt='trois-logo' />
                     </div>
                     <div className='grid-item'>
-                        <img src={unileven} alt='unileven-logo' />
+                        <img src={acaso} alt='acaso-logo' />
                     </div>
                 </div>
             </div>
