b8b8575a4ec5b8ac793bb76019ed6c53d4bf2792
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-04-30 10:24:19 -0300

    fix: fonte em solucoes home

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:
+  {
+    host: true, // Open to local network and display URL
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+    port: 3001
+  },
   plugins: [react()],
 })
